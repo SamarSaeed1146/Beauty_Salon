4762fdfb77e537f271facd69b0857f12921dd373
SamarSaeed1146 <samarsaeed374@gmail.com>
2025-03-06 04:08:23 +0500

    Add methods

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com", // Google profile images
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com", // GitHub profile images
+      },
+      {
+        protocol: "https",
+        hostname: "media.licdn.com", // LinkedIn profile images
+      },
+    ],
+  },
 };
 
 export default nextConfig;
